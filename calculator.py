3a46002210e9c819b854601a4aef822c5f282b3a
kashishgupta748 <kashihsgupta892004@gmail.com>
2024-05-01 21:12:06 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+a=int(input("enter the number "))
+b=int(input("enter the number "))
+choice = input("enter the choice 1/2/3/4/5  5")
+
+if choice == '1':
+    print("the additon of two numbers is: ",a+b)
+elif choice == '2':
+    print("the subtraction of two numbers is: ", a-b)
+elif choice == '3':
+    print("the multiplication of two numbers is: ",a*b)
+elif choice == '4':
+    print("the division of two numbers is: ",a/b)
+elif choice == '5':
+    print("the floor division of two numbers is: ",a//b)
+else:
+    print("invalid input")
+
+
+
+
+ 
